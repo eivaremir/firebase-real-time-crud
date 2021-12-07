c52c56bfda197a8823f542d9ea9a94ac2387cc84
Eivar Morales <eivaremir@gmail.com>
2021-12-07 16:47:19 -0500

    Added: Online status

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './App.css';
-import Form from './components/Form';
-import TodoList from './components/TodoList';
-import UploadImage from './components/UploadImage';
-
+import React, { useEffect } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import UploadImage from "./components/UploadImage";
+import firebase from "./util/firebase";
 export default function App() {
+  useEffect(() => {
+    firebase.database().ref("Todo").child("user").update({
+      online: true,
+    });
+
+    window.addEventListener("beforeunload", (ev) => {
+      ev.preventDefault();
+      firebase.database().ref("Todo").child("user").update({
+        online: false,
+      });
+    });
+  }, []);
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      onBeforeUnload={(e) => {
+        e.preventDefault();
+        alert("ah?");
+      }}
+    >
       <h1>Todo</h1>
       <Form />
       <TodoList />
